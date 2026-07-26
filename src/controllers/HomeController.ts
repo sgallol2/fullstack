c06103007b80a2a6f2b8722f55d9cbdbbe4229c3
sgallo <sofiagdelarosa@gmail.com>
2026-07-26 13:49:34 -0500

    First tutorial

---
+++ src/controllers/HomeController.ts
@@ -0,0 +1,18 @@
+import type { Request, Response } from 'express';
+
+export class HomeController {
+  static index(req: Request, res: Response): void {
+    const viewData: { [key: string]: any } = {};
+    viewData["title"] = "Home";
+
+    res.render('home/index', { viewData: viewData });
+  }
+
+  static about(req: Request, res: Response): void {
+    const viewData: { [key: string]: any } = {};
+    viewData["title"] = "About";
+
+    res.render('home/about', { viewData: viewData });
+  }
+
+}
---
+++ src/routes/Routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { HomeController } from '../controllers/HomeController.js';
+
+export default class Routes {
+  static initializeRoutes(): Router {
+    const router = Router();
+    
+    router.get('/', HomeController.index);
+    router.get('/about', HomeController.about);
+    return router;
+  }
+}
